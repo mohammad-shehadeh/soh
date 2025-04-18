43648c37554af120ad8a7cec2cba0726b5d14311
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-18 22:10:15 +0300

    تحديث البيانات من المحرر

--- data.js
+++ data.js
@@ -1,44 +1,43 @@
-// data.js
 const categories = [
-    {
-        name: "مصابيح ليد",
-        image: "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg" // URL صورة الفئة
-    },
-    {
-        name: "إضاءة خارجية",
-        image: "https://cdn.salla.sa/KjYjdb/0lYORcT8H8NPYnGNp1sQbYZfLT6DSnWdD2TFiPTD.jpg"
-    },
-    {
-        name: "إضاءة داخلية",
-        image: "https://cdn.salla.sa/WRneP/JIWbcJ7b4MZuYJMXNnp9ifrb18AmvbTGlDIEglI1.jpg"
-    },
-    {
-        name: "إضاءة شوارع",
-        image: "https://www.tarshid.com.sa/_next/image?url=https%3A%2F%2Fwww.tarshid.com.sa%2Fuploads%2Fmain_cover.jpg-nrps7n.jpeg&w=3840&q=75"
-    },
-    {
-        name: "إضاءة مكتبية",
-        image: "https://m.media-amazon.com/images/I/61jl27ilxtL.jpg"
-    }
+  {
+    "name": "مصابيح ليد",
+    "image": "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
+  },
+  {
+    "name": "إضاءة خارجية",
+    "image": "https://cdn.salla.sa/KjYjdb/0lYORcT8H8NPYnGNp1sQbYZfLT6DSnWdD2TFiPTD.jpg"
+  },
+  {
+    "name": "إضاءة داخلية",
+    "image": "https://cdn.salla.sa/WRneP/JIWbcJ7b4MZuYJMXNnp9ifrb18AmvbTGlDIEglI1.jpg"
+  },
+  {
+    "name": "إضاءة شوارع",
+    "image": "https://www.tarshid.com.sa/_next/image?url=https%3A%2F%2Fwww.tarshid.com.sa%2Fuploads%2Fmain_cover.jpg-nrps7n.jpeg&w=3840&q=75"
+  },
+  {
+    "name": "إضاءة مكتبية",
+    "image": "https://m.media-amazon.com/images/I/61jl27ilxtL.jpg"
+  }
 ];
 
 const products = [
-    {
-        category: "مصابيح ليد",
-        name: "مصباح ليد 10 واط",
-        price: 15.99,
-        image: "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
-    },
-    {
-        category: "إضاءة خارجية",
-        name: "مصباح حديقة مقاوم للماء",
-        price: 34.50,
-        image: "https://example.com/garden-light.jpg"
-    },
-    {
-        category: "إضاءة مكتبية",
-        name: "مصباح مكتب LED",
-        price: 45.75,
-        image: "https://example.com/desk-lamp.jpg"
-    }
+  {
+    "category": "مصابيح ليد",
+    "name": "مصباح ليد 10 واط",
+    "price": 99.99,
+    "image": "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
+  },
+  {
+    "category": "إضاءة خارجية",
+    "name": "مصباح حديقة مقاوم للماء",
+    "price": 34.5,
+    "image": "https://example.com/garden-light.jpg"
+  },
+  {
+    "category": "إضاءة مكتبية",
+    "name": "مصباح مكتب LED",
+    "price": 45.75,
+    "image": "https://example.com/desk-lamp.jpg"
+  }
 ];
