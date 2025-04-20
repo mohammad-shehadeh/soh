2db98ad81828a86b93c7960009252bb6810e29c9
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-20 10:53:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ document.addEventListener('DOMContentLoaded', () => {
         productCard.className = 'product-card';
 
         productCard.innerHTML = `
-            ${hasDiscount ? `<div class="discount-label">تخفيض</div>` : ''}
+            ${hasDiscount ? `<div class="discount-label">خصم</div>` : ''}
             <img src="${product.image}" alt="${product.name}">
             <h3>${product.name}</h3>
             <p class="price ${hasDiscount ? 'on-sale' : (isAvailable ? '' : 'unavailable')}"
