7b0be4a4d9df83bd75ad49de512b405f720d40ab
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-21 21:37:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 const TOKEN_PART1 = "ghp_UCTgHQsAY5MjZ9AFye5";
 const TOKEN_PART2 = "NPKty4Z4nt62H5cta";
 const GITHUB_TOKEN = TOKEN_PART1 + TOKEN_PART2;
+
+// Cloudinary Config
+const CLOUDINARY_CLOUD_NAME = "dygc5b8cn"; // غيّرها إذا اختلف اسم الحساب
+const CLOUDINARY_UPLOAD_PRESET = "Shehadeh"; // تأكد من أنه Unsigned
