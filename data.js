2593ff80205592d7c2b43355b43d02ededf10af7
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-26 10:34:15 +0300

    تحديث البيانات من المحرر

--- data.js
+++ data.js
@@ -1,6 +1,30 @@
 const categories = [
   {
-    "name": "كرستال",
+    "name": "انارة سلندر",
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745652835/ked0qekaxuaqr24ky1xd.jpg"
+  },
+  {
+    "name": "جلوبات سقف",
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745652710/rtfdnetu2eaebc2cew1a.jpg"
+  },
+  {
+    "name": "UP &DOWN IP65",
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745652645/eer14xwb3rhwxgk3louv.jpg"
+  },
+  {
+    "name": "سبوت خارجي (بلا حفر)",
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745652586/xzjzardcetve1jxdverx.jpg"
+  },
+  {
+    "name": "انارة لينير",
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745652490/ywof3iwwjfjwnypoc8w4.jpg"
+  },
+  {
+    "name": "سبوت ليد داخلي",
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745651988/gfcau9jje73ugwwncoip.jpg"
+  },
+  {
+    "name": "كرستال موديرن",
     "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745263564/v5hfdr3kmdmewvlfuchn.jpg"
   },
   {
@@ -16,37 +40,37 @@ const categories = [
     "image": "https://i.postimg.cc/Qd3DwGhg/IMG-7305.jpg"
   },
   {
-    "name": "تريات كرستال",
+    "name": "ثريا كريستال كلاسيك",
     "image": "https://m.media-amazon.com/images/I/71gTDrRvIOL._AC_UF1000,1000_QL80_.jpg"
   },
   {
-    "name": "مصابيح ليد",
+    "name": "شريط الانارة المخفية 220 فولت",
     "image": "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
   },
   {
-    "name": "إضاءة خارجية",
+    "name": "إضاءة الحدائق",
     "image": "https://cdn.salla.sa/KjYjdb/0lYORcT8H8NPYnGNp1sQbYZfLT6DSnWdD2TFiPTD.jpg"
   },
   {
-    "name": "إضاءة داخلية",
+    "name": "إضاءة جانبية داخلية",
     "image": "https://cdn.salla.sa/WRneP/JIWbcJ7b4MZuYJMXNnp9ifrb18AmvbTGlDIEglI1.jpg"
   },
   {
-    "name": "إضاءة مكتبية",
+    "name": "تيبل لامب",
     "image": "https://m.media-amazon.com/images/I/61jl27ilxtL.jpg"
   }
 ];
 
 const products = [
   {
-    "category": "كرستال",
+    "category": "كرستال موديرن",
     "name": "سايد لامب كريستال P6022-6",
     "price": 1300,
     "discountedPrice": null,
     "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745651866/sb6iipofsj3tqrv661vh.jpg"
   },
   {
-    "category": "تريات كرستال",
+    "category": "ثريا كريستال كلاسيك",
     "name": "ثريا كرستال",
     "price": 1700,
     "discountedPrice": 2200,
@@ -59,21 +83,21 @@ const products = [
     "image": "https://m.media-amazon.com/images/I/71nrcOr+VpL._AC_SL1500_.jpg"
   },
   {
-    "category": "كرستال",
+    "category": "كرستال موديرن",
     "name": "مصباح ليد 10 واط",
     "price": 70,
     "discountedPrice": 140,
     "image": "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
   },
   {
-    "category": "كرستال",
+    "category": "كرستال موديرن",
     "name": "مصباح حديقة مقاوم للماء",
     "price": 44.5,
     "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745307709/sif2kddhsab2goktrmdp.jpg",
     "discountedPrice": 50
   },
   {
-    "category": "إضاءة مكتبية",
+    "category": "تيبل لامب",
     "name": "مصباح مكتب LED",
     "price": 45.75,
     "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745269296/mtszbxef5jbt9k7aq1av.jpg"
